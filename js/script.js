9bdc0a9094edbe49c5cf3905ecd234a8e87274b1
Penguin27564 <penguin27564@gmail.com>
2023-08-06 23:21:06 +0800

    finished most of website

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 //#region Healthy Plate
 var OpenPlateBtn = document.querySelector("#toggle-plate-btn");
 var plate = document.querySelector("#plate-container");
+var plateBody = document.querySelector("#plate-container-body");
 var isOpen = false;
 
 function HidePlate()
@@ -22,7 +23,6 @@ function ShowPlate()
     else
     {
         OpenPlateBtn.textContent = "Close";
-        plate.style.display = "flex";
     }
     isOpen = !isOpen;
 }
@@ -30,6 +30,7 @@ function ShowPlate()
 OpenPlateBtn.addEventListener("click", function () 
     {
         ShowPlate();
+        plateBody.classList.toggle("fade");
     }
 );
 //#endregion
@@ -115,4 +116,86 @@ function ToggleSection(ID)
 }
 
 ToggleSection(0);
+// #endregion
+
+// #region Macros Form
+// To toggle the macros form
+var macros = document.getElementById("macros");
+var macrosOpen = false;
+
+document.getElementById("macrosbtn").onclick = function()
+{
+    macros.classList.toggle("fade");
+    if (!macrosOpen)
+    {
+        macros.style.display = "block";
+        macrosOpen = true;
+    }
+    else
+    {
+        macros.style.display = "none";
+        macrosOpen = false;
+    }
+};
+
+var bmrResult = document.querySelector("#bmr-result");
+var dacResult = document.querySelector("#dac-result");
+var splitResult = document.querySelector("#split-result");
+var genderSelect = document.querySelector("#gender-form");
+var activitySelect = document.querySelector("#activity-form");
+var form = document.querySelector("#macros-form");
+var multipler;
+var BMR;
+var DAC;
+var proteinAndCarbs;
+var fats;
+//var macrosResult = document.querySelector("#macros-results");
+
+function displayResults()
+{
+    const weight = form.weight.value;
+    const height = form.height.value;
+    const age = form.age.value;
+
+    if (activitySelect.value == "Little to None")
+    {
+        multipler = 1.2;
+    }
+    else if (activitySelect.value == "Light, few times a week")
+    {
+        multipler = 1.375;
+    }
+    else if (activitySelect.value == "Moderate, 3-5 times a week")
+    {
+        multipler = 1.55;
+    }
+    else if (activitySelect.value == "Heavy, 6-7 times a week")
+    {
+        multipler = 1.725;
+    }
+
+    if (genderSelect.value == "Male")
+    {
+        BMR = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
+    }
+    else if (genderSelect.value == "Female")
+    {
+        BMR = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+    }
+    else
+    {
+        BMR = 0;
+    }
+
+    DAC = BMR * multipler;
+
+    proteinAndCarbs = DAC * 0.4;
+    fats = DAC * 0.2;
+
+    bmrResult.textContent = "Your BMR is: " + BMR.toFixed(2);
+    dacResult.textContent = "Your DAC is: " + DAC.toFixed(2);
+    splitResult.textContent = "Protein: " + proteinAndCarbs.toFixed(2) + "g" + '\n' +
+                              "Carbohydrates: " + proteinAndCarbs.toFixed(2) + "g" + '\n' +
+                              "Fats: " + fats.toFixed(2) + "g";
+}
 // #endregion
